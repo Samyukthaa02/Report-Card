dea21cababc9f902b5c33b43ca02117e5bbeeb7c
Samyukthaa02 <141002483+Samyukthaa02@users.noreply.github.com>
2026-01-10 16:05:46 +0530

    "removed csrf"

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt
 
 from checkout.webhook_handler import StripeWH_Handler
 
@@ -9,7 +8,7 @@ import stripe
 
 
 @require_POST
-@csrf_exempt
+
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
